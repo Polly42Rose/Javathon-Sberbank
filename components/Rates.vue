f3c9e05e3ccd54f5ee51d3600e12bbc225ed2e9e
green_tea <vetlin.vladislav@yandex.ru>
2019-01-26 21:15:22 +0300

    some fixes in rate

--- components/Rates.vue
+++ components/Rates.vue
@@ -1,20 +1,25 @@
 <template>
     <div class="rate-container">
         <div class="rate-title">Курсы валют</div>
-        <table>
+        <table class="rate-table-style">
             <tr>
                 <th></th>
                 <th class="rate-table-title">Курс</th>
             </tr>
-            <tr class="rate-th-style">
+            <tr>
                 <th>Евро</th>
                 <th>{{euro}}</th>
+                <th v-if="isEuroUp" class="up-rate-arrow"></th>
+                <th v-else class="down-rate-arrow"></th>
             </tr>
-            <tr class="rate-th-style">
+            <tr>
                 <th>Доллар США</th>
                 <th>{{dollar}}</th>
+                <th v-if="isDollarUp" class="up-rate-arrow"></th>
+                <th v-else class="down-rate-arrow"></th>
             </tr>
         </table>
+        <div class="rate-exchange-url"><a href="/test">Обмен валюты</a></div>
     </div>
 </template>
 
@@ -25,6 +30,8 @@ export default {
         return {
             euro: 73.62,
             dollar: 64.96,
+            isEuroUp: false,
+            isDollarUp: true
         }
     }
 }
@@ -32,22 +39,54 @@ export default {
 
 <style lang="scss">
 
+    %image-style {
+        width: 3px;
+        height: 3px;
+        background-size: contain;
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+
     .rate-container {
         margin-left: 15vw;
+        width: 10vw;
 
         .rate-title {
             font-weight: bold;
             font-size: 16px;
-            margin-bottom: 1em;
+            margin-bottom: 1vh;
         }
 
-        .rate-table-title {
+        .rate-table-style {
+            width: 100%;
+
+            .rate-table-title {
+                opacity: .5;
+            }
+
+            .down-rate-arrow {
+                @extend %image-style;
+                background-image: url('/static/rates/DownArrow.png');
+            }
+
+            .up-rate-arrow {
+                @extend %image-style;
+                background-image: url('/static/rates/UpArrow.png');
+            }
+
+            th {
+                padding-right: .5em;
+                font-weight: normal;
+            }
+        }
+
+        .rate-exchange-url {
+            display: flex;
+            margin-top: 1vh;
             opacity: .5;
-        }
-
-        th {
-            padding-right: 1em;
-            font-weight: normal;
+            color: white;
+            justify-content: flex-end;
         }
     }
+
 </style>
